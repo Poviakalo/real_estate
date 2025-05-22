3b65ff57a49476b0ea33519c08fd54d4b1e8e977
Sashko <oleksandr.poviakalo@gmail.com>
2025-05-22 13:40:12 +0300

    Create update user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 
 export const router = express.Router();
 
 router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser);
