76ae7b3ab0ecfc70c683e2a38b99e891091d7ba1
Sashko <oleksandr.poviakalo@gmail.com>
2025-08-27 19:32:58 +0300

    Added  secure api key

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAPTrsjedHWRLM1eqv7ML7gACsuinFQj2U",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "real-estate-e61a3.firebaseapp.com",
   projectId: "real-estate-e61a3",
   storageBucket: "real-estate-e61a3.firebasestorage.app",
